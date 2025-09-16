81664214f8a794a68839a28765b6c2d8b324f4cf
user <danikmamaiko@gmail.com>
2025-09-16 21:20:06 +0300

    add-events-parsed-page

--- src/components/ui/pagination.tsx
+++ src/components/ui/pagination.tsx
@@ -49,14 +49,14 @@ PaginationLink.displayName = "PaginationLink";
 const PaginationPrevious = ({ className, ...props }: React.ComponentProps<typeof PaginationLink>) => (
   <PaginationLink aria-label="Go to previous page" size="default" className={cn("gap-1 pl-2.5", className)} {...props}>
     <ChevronLeft className="h-4 w-4" />
-    <span>Previous</span>
+    <span>Назад</span>
   </PaginationLink>
 );
 PaginationPrevious.displayName = "PaginationPrevious";
 
 const PaginationNext = ({ className, ...props }: React.ComponentProps<typeof PaginationLink>) => (
   <PaginationLink aria-label="Go to next page" size="default" className={cn("gap-1 pr-2.5", className)} {...props}>
-    <span>Next</span>
+    <span>Вперед</span>
     <ChevronRight className="h-4 w-4" />
   </PaginationLink>
 );
